641abed5d8dbf6e8a7096cc217fb8d63d06e192f
rpurciel <rpurciel@gmail.com>
2023-12-19 16:07:02 -0800

    Removed debug logging from run script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Logs everything
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>~/logs/contrail-hunters.log 2>&1
+# exec 3>&1 4>&2
+# trap 'exec 2>&4 1>&3' 0 1 2 3
+# exec 1>~/logs/contrail-hunters.log 2>&1
 
 # Run using:
 # bash -i ~/perlan_data/scripts/GFS_wrapper.sh
